86bfef1db8a579a9b391b3eded06b20af6a61d9a
masa <b77yellow@gmail.com>
2026-07-09 22:04:49 +0900

    supabase settting

---
+++ src/utils/supabase.ts
@@ -0,0 +1,9 @@
+import { createClient } from "@supabase/supabase-js";
+
+// Create a single supabase client for interacting with your database
+// eslint-disable-next-line no-unused-vars
+export const supabase = createClient(
+    import.meta.env.VITE_SUPABASE_URL,
+    //   import.meta.env.VITE_SUPABASE_URL,
+    import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY,
+);
---
+++ src/utils/supabaseFunction.ts
@@ -0,0 +1,35 @@
+import { supabase } from "./supabase";
+
+// データ取得(全件)
+export const getAllRecords = async () => {
+    const records = await supabase.from("study-record").select("*");
+    return records;
+};
+
+// データの登録
+export const insertRecord = async (title, time) => {
+    const { error } = await supabase.from("study-record").insert([
+        {
+            title,
+            time,
+        },
+    ]);
+
+    if (error) {
+        console.error(error);
+        return;
+    }
+};
+
+// データの削除
+export const deleteRecord = async (index) => {
+    console.log(index);
+    const { error } = await supabase
+        .from("study-record")
+        .delete()
+        .eq("id", index);
+    console.log(error);
+    if (error) {
+        console.error(error);
+    }
+};
